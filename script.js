6063001b483e0c08f3e514b7d8810501764cc3e8
HVietAnh <132793306+HVietAnh@users.noreply.github.com>
2023-05-10 18:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function clearScreen() {
-    document.getElementById("result").value = "";
-}
-
-
-// This function display values
 function display(value) {
-    document.getElementById("result").value += value;
+document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and return result
 function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+var exp = document.getElementById("result").value;
+if (exp) {
+document.getElementById("result").value = eval(exp) ;
+}
+}
+
+function clearScreen() {
+document.getElementById("result").value = "";
 }
